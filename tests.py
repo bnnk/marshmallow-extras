c70917883c08a65f957c2c1ca3fac16d17ac59fe
bnnk <50262917+bnnk@users.noreply.github.com>
2020-08-19 23:14:25 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -19,8 +19,8 @@ ent = dict(
     df = pd.DataFrame({"a" : ["12"], "B" : ["13"]}),
     arr = np.array([1,2,3,4,5]),
     req = get("http://google.com"),
-    ho = pyotp.hotp.HOTP(pyotp.randome_base32()),
-    to = pyotp.totp.TOTP(pyotp.randome_base32())
+    ho = pyotp.hotp.HOTP(pyotp.random_base32()),
+    to = pyotp.totp.TOTP(pyotp.random_base32())
 )
 print(TestingSchema().dump(ent))
 print(TestingSchema().load(TestingSchema().dump(ent)))
