af3eade6b6f314341f6e36e0a813a8836be4df09
bnnk <50262917+bnnk@users.noreply.github.com>
2020-08-19 23:06:57 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -16,7 +16,7 @@ class TestingSchema(Schema):
 ent = dict(
     furl = furl("bz2://"),
     df = pd.DataFrame({"a" : ["12"], "B" : ["13"]}),
-    arr = np.array([1,2,3,4,5])
+    arr = np.array([1,2,3,4,5]),
     req = get("http://google.com"),
     ho = pyotp.hotp.HOTP(pyotp.randome_base32()),
     to = pyotp.totp.TOTP(pyotp.randome_base32())
